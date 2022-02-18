75765b995aab76285f1b3c6ee7553ddbb465edd7
Sabbir Hossain <35090483+sabbir-rupom@users.noreply.github.com>
2022-02-18 22:32:52 +0600

    source code initialized

---
+++ README.md
@@ -0,0 +1,1 @@
+# laravel-reactJS-bootstrap-crud
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Avoid database unique field naming schema error
+        Schema::defaultStringLength(191);
+
+        // Use bootstrap CSS as default pagination UI
+        Paginator::useBootstrap();
+    }
+}
